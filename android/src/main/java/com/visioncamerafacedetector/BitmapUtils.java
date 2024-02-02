f995ffe209946288bdbc8ad4dab5948615a5324e
Luiz Carlos <nonam4@yahoo.com>
2024-02-02 11:09:37 -0300

    fix missing brackets

--- android/src/main/java/com/visioncamerafacedetector/BitmapUtils.java
+++ android/src/main/java/com/visioncamerafacedetector/BitmapUtils.java
@@ -34,44 +34,45 @@ public class BitmapUtils {
   private static final String TAG = "BitmapUtils";
 
   public static Bitmap convertImageToBitmap(InputImage image) {
-        Image.Plane[] planes = image.getPlanes();
+    Image.Plane[] planes = image.getPlanes();
         
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
+    ByteBuffer yBuffer = planes[0].getBuffer();
+    ByteBuffer uBuffer = planes[1].getBuffer();
+    ByteBuffer vBuffer = planes[2].getBuffer();
 
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
+    int ySize = yBuffer.remaining();
+    int uSize = uBuffer.remaining();
+    int vSize = vBuffer.remaining();
 
-        byte[] nv21 = new byte[ySize + uSize + vSize];
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
+    byte[] nv21 = new byte[ySize + uSize + vSize];
+    //U and V are swapped
+    yBuffer.get(nv21, 0, ySize);
+    vBuffer.get(nv21, ySize, vSize);
+    uBuffer.get(nv21, ySize + vSize, uSize);
 
-        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 90, out);
+    YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 90, out);
 
-        byte[] imageBytes = out.toByteArray();
-        Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-        Matrix matrix = new Matrix();
-        matrix.postRotate(-90);
-        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    byte[] imageBytes = out.toByteArray();
+    Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    Matrix matrix = new Matrix();
+    matrix.postRotate(-90);
+    Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
-        return rotatedBitmap;
+    return rotatedBitmap;
   }
 
   public static int convertRotationDegreeFromString(String orientation){
     switch (orientation){
-        case "portrait-upside-down":
-            return 180;
-        case "landscape-left":
-            return 90;
-        case "landscape-right":
-            return 270;
-        default:
-            return 0;
+      case "portrait-upside-down":
+        return 180;
+      case "landscape-left":
+        return 90;
+      case "landscape-right":
+        return 270;
+      default:
+        return 0;
     }
+  }
 }
