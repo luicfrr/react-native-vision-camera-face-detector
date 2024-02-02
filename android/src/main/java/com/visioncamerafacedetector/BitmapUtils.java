8cc83050f0189b0a8784420f9c54e084d6c4417c
Luiz Carlos <nonam4@yahoo.com>
2024-02-02 11:19:09 -0300

    fix bitmat conversion

--- android/src/main/java/com/visioncamerafacedetector/BitmapUtils.java
+++ android/src/main/java/com/visioncamerafacedetector/BitmapUtils.java
@@ -1,30 +1,13 @@
 package com.visioncamerafacedetector;
 
-/*
- * Copyright 2020 Google LLC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.ImageFormat;
-import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
 import android.media.Image;
-
-import com.google.mlkit.vision.common.InputImage;
+import android.media.Image.Plane;
+import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
@@ -33,46 +16,45 @@ import java.nio.ByteBuffer;
 public class BitmapUtils {
   private static final String TAG = "BitmapUtils";
 
-  public static Bitmap convertImageToBitmap(InputImage image) {
+  private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+  public static String convertYuvToRgba(Image image) {
     Image.Plane[] planes = image.getPlanes();
-        
+    int width = image.getWidth();
+    int height = image.getHeight();
+
     ByteBuffer yBuffer = planes[0].getBuffer();
-    ByteBuffer uBuffer = planes[1].getBuffer();
-    ByteBuffer vBuffer = planes[2].getBuffer();
+    ByteBuffer uvBuffer = planes[1].getBuffer();
 
     int ySize = yBuffer.remaining();
-    int uSize = uBuffer.remaining();
-    int vSize = vBuffer.remaining();
+    int uvSize = uvBuffer.remaining();
 
-    byte[] nv21 = new byte[ySize + uSize + vSize];
-    //U and V are swapped
-    yBuffer.get(nv21, 0, ySize);
-    vBuffer.get(nv21, ySize, vSize);
-    uBuffer.get(nv21, ySize + vSize, uSize);
+    byte[] nv21Data = new byte[ySize + uvSize];
 
-    YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 90, out);
+    // Copy Y plane
+    yBuffer.get(nv21Data, 0, ySize);
 
-    byte[] imageBytes = out.toByteArray();
-    Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-    Matrix matrix = new Matrix();
-    matrix.postRotate(-90);
-    Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    // Copy interleaved U and V planes
+    uvBuffer.get(nv21Data, ySize, uvSize);
 
-    return rotatedBitmap;
-  }
+    // Convert NV21 to RGB
+    int[] rgbaArray = new int[width * height];
 
-  public static int convertRotationDegreeFromString(String orientation){
-    switch (orientation){
-      case "portrait-upside-down":
-        return 180;
-      case "landscape-left":
-        return 90;
-      case "landscape-right":
-        return 270;
-      default:
-        return 0;
-    }
+    outputStream.reset();
+    YuvImage yuvImage = new YuvImage(nv21Data, ImageFormat.NV21, width, height, null);
+    yuvImage.compressToJpeg(new Rect(0, 0, width, height), 90, outputStream);
+
+    byte[] jpegData = outputStream.toByteArray();
+    Bitmap bitmap = BitmapFactory.decodeByteArray(jpegData, 0, jpegData.length);
+
+    // Get RGB values from the bitmap
+    bitmap.getPixels(rgbaArray, 0, width, 0, 0, width, height);
+
+    // Create Bitmap from RGB array
+    Bitmap rgbaBitmap = Bitmap.createBitmap(rgbaArray, width, height, Bitmap.Config.RGB_565);
+
+    outputStream.reset();
+    rgbaBitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+    return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
   }
 }
