f7990bb1b1915c942c9f724297949d412ef50762
Luiz Carlos <nonam4@yahoo.com>
2024-02-07 11:51:02 -0300

    fix plugin declaration

--- ios/VisionCameraFaceDetector.m
+++ ios/VisionCameraFaceDetector.m
@@ -1,6 +1,22 @@
+#import <Foundation/Foundation.h>
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/FrameProcessorPluginRegistry.h>
+#import <VisionCamera/Frame.h>
 
+#if __has_include("VisionCameraFaceDetector/VisionCameraFaceDetector-Swift.h")
+#import "VisionCameraFaceDetector/VisionCameraFaceDetector-Swift.h"
+#else
 #import "VisionCameraFaceDetector-Swift.h"
+#endif
 
-VISION_EXPORT_SWIFT_FRAME_PROCESSOR(VisionCameraFaceDetector, detectFaces)
+@interface VisionCameraFaceDetector (FrameProcessorPluginLoader)
+@end
+
+@implementation VisionCameraFaceDetector (FrameProcessorPluginLoader)
++ (void)initialize{
+  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"detectFaces"
+    withInitializer:^FrameProcessorPlugin*(VisionCameraProxyHolder* proxy, NSDictionary* options) {
+    return [[VisionCameraFaceDetector alloc] initWithProxy:proxy withOptions:options];
+  }];
+}
+@end
