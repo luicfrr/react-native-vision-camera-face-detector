f4f78b84c8f3126a8194c7c8bd45975dcd68c811
Luiz Carlos <nonam4@yahoo.com>
2024-03-27 10:40:41 -0300

    lint

--- example/babel.config.js
+++ example/babel.config.js
@@ -12,10 +12,11 @@ module.exports = {
         '.js',
         '.json'
       ]
-    } ],
-  [ 'react-native-reanimated/plugin', {
-    processNestedWorklets: true
-  } ],
-  [ 'react-native-worklets-core/plugin' ]
-  ]
+    } ], [
+    'react-native-reanimated/plugin', {
+      processNestedWorklets: true
+    }
+  ], [
+    'react-native-worklets-core/plugin'
+  ] ]
 }
