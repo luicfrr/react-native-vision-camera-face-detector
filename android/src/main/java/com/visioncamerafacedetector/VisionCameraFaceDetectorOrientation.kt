accc46d3ebebaa6d3573396601be9d9f8116251c
Luiz Carlos <luicfrr+github@gmail.com>
2025-04-29 18:40:36 -0300

    fix #147

--- android/src/main/java/com/visioncamerafacedetector/VisionCameraFaceDetectorOrientation.kt
+++ android/src/main/java/com/visioncamerafacedetector/VisionCameraFaceDetectorOrientation.kt
@@ -1,18 +1,25 @@
 package com.visioncamerafacedetector
 
 import android.content.Context
+import android.os.Build
 import android.view.OrientationEventListener
 import android.view.Surface
+import androidx.annotation.RequiresApi
 
+@RequiresApi(Build.VERSION_CODES.CUPCAKE)
 class VisionCameraFaceDetectorOrientation(private val context: Context) {
     var orientation = Surface.ROTATION_0
-    private val orientationListener = object : OrientationEventListener(context) {
-        override fun onOrientationChanged(rotationDegrees: Int) {
-            orientation = degreesToSurfaceRotation(rotationDegrees)
-        }
-    }
+    private var orientationListener: OrientationEventListener? = null
 
     init {
+        if (orientationListener == null) {
+            orientationListener = object : OrientationEventListener(context) {
+                override fun onOrientationChanged(rotationDegrees: Int) {
+                    orientation = degreesToSurfaceRotation(rotationDegrees)
+                }
+            }
+        }
+
         orientation = Surface.ROTATION_0
         startDeviceOrientationListener()
     }
@@ -24,13 +31,20 @@ class VisionCameraFaceDetectorOrientation(private val context: Context) {
     private fun startDeviceOrientationListener() {
         stopDeviceOrientationListener()
 
-        if(orientationListener.canDetectOrientation()) {
-            orientationListener.enable()
+        if (
+            orientationListener != null &&
+            orientationListener!!.canDetectOrientation()
+        ) {
+            println("Enabling device orientation listener")
+            orientationListener!!.enable()
         }
     }
 
     private fun stopDeviceOrientationListener() {
-        orientationListener.disable()
+        if (orientationListener != null) {
+            println("Disabled device orientation listener")
+            orientationListener!!.disable()
+        }
     }
 
     private fun degreesToSurfaceRotation(degrees: Int): Int =
