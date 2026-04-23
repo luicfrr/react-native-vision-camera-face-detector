dd2d924c3adec54d8d06798c790b8e842e11ec28
Luiz Carlos <60632638+luicfrr@users.noreply.github.com>
2026-04-23 08:07:46 -0300

    will not provide output hook

--- src/factory.ts
+++ src/factory.ts
@@ -1,15 +1,10 @@
 import { NitroModules } from 'react-native-nitro-modules'
-import type {
-  CameraOutput,
-  CameraSession,
-  Frame,
-} from 'react-native-vision-camera'
+import type { Frame } from 'react-native-vision-camera'
 import type { Face } from './specs/Face.nitro'
 import type { FaceDetector } from './specs/FaceDetector.nitro'
 import type {
   FaceDetectorFactory,
-  FrameFaceDetectorOptions,
-  FaceDetectorOutputOptions,
+  FrameFaceDetectorOptions
 } from './specs/FaceDetectorFactory.nitro'
 
 const factory = NitroModules.createHybridObject<FaceDetectorFactory>(
@@ -26,16 +21,4 @@ export function createFaceDetector(
   options?: FrameFaceDetectorOptions
 ): FaceDetector {
   return factory.createFaceDetector( options )
-}
-
-/**
- * Create a new Face Detector {@linkcode CameraOutput}.
- *
- * The Face Detector {@linkcode CameraOutput} can be
- * attached to a {@linkcode CameraSession}.
- */
-export function createFaceDetectorOutput(
-  options?: FaceDetectorOutputOptions,
-): CameraOutput {
-  return factory.createFaceDetectorOutput( options )
 }
--- src/specs/FaceDetectorCallback.ts
+++
@@ -1,7 +0,0 @@
-import type { Frame } from "react-native-vision-camera"
-import type { Face } from "./Face.nitro"
-
-export type FaceDetectorCallback = (
-  faces: Face[],
-  frame: Frame
-) => void | Promise<void>
