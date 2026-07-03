4022012f8791141ecf079f40815639b7e09cb3f6
Luiz Carlos <60632638+luicfrr@users.noreply.github.com>
2026-07-03 15:51:30 -0300

    fix wrong symbol

--- android/src/main/java/com/margelo/nitro/camera/facedetector/HybridFaceDetectorOutput.kt
+++ android/src/main/java/com/margelo/nitro/camera/facedetector/HybridFaceDetectorOutput.kt
@@ -30,7 +30,7 @@ class HybridFaceDetectorOutput(
   override val mediaType: MediaType = MediaType.VIDEO
   override val mirrorMode: MirrorMode = MirrorMode.AUTO
   override var outputOrientation: CameraOrientation = CameraOrientation.UP
-    get() - field
+    get() = field
     set(value) {
       field = value
       imageAnalysis?.targetRotation = value.surfaceRotation
