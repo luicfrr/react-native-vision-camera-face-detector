361d03c7ce62a52cb30986cc26c250995f14f60d
Shubhanshu Kumar <shubhanshubb@gmail.com>
2026-09-17 23:25:07 +0530

    feat(ios): detect image faces with Apple Vision

--- ios/HybridImageFaceDetector.swift
+++ ios/HybridImageFaceDetector.swift
@@ -1,37 +1,34 @@
 import Foundation
-import MLKitFaceDetection
-import MLKitVision
+import NitroModules
+import UIKit
+import Vision
 
 class HybridImageFaceDetector: HybridImageFaceDetectorSpec {
   private let runLandmarks: Bool
   private let runContours: Bool
   private let runClassifications: Bool
   private let trackingEnabled: Bool
-  private let faceDetector: FaceDetector
 
   init(_ options: ImageFaceDetectorOptions) {
     self.runLandmarks = options.runLandmarks ?? false
     self.runContours = options.runContours ?? false
     self.runClassifications = options.runClassifications ?? false
     self.trackingEnabled = options.trackingEnabled ?? false
-    self.faceDetector = FaceDetector.faceDetector(
-      options: options.toMLFaceDetectorOptions()
-    )
-
     super.init()
   }
 
-  func detectFaces(
-    image: InputImage
-  ) throws -> [any HybridFaceSpec] {
-    let uiImage = try createInputImage(
-      uri: try resolveInputImage(image)
-    )
-    let mlImage = VisionImage(image: uiImage)
-    // ML Kit face coordinates are expressed in image pixels, not UIKit points.
-    let frameWidth = Double(uiImage.cgImage?.width ?? Int(uiImage.size.width * uiImage.scale))
-    let frameHeight = Double(uiImage.cgImage?.height ?? Int(uiImage.size.height * uiImage.scale))
+  func detectFaces(image: InputImage) throws -> [any HybridFaceSpec] {
+    let uiImage = try createInputImage(uri: try resolveInputImage(image))
+    guard let cgImage = uiImage.cgImage else {
+      throw RuntimeError.error(withMessage: "Failed to create CGImage from input image!")
+    }
 
+    let request = VNDetectFaceLandmarksRequest()
+    let handler = VNImageRequestHandler(cgImage: cgImage, orientation: .up, options: [:])
+    try handler.perform([request])
+
+    let frameWidth = Double(cgImage.width)
+    let frameHeight = Double(cgImage.height)
     let config = createFaceProcessConfig(
       frameWidth,
       frameHeight,
@@ -43,83 +40,24 @@ class HybridImageFaceDetector: HybridImageFaceDetectorSpec {
       createIdentityPointTransformer()
     )
 
-    let faces = try faceDetector.results(in: mlImage)
-    return faces.map {
-      HybridFace(
-        face: $0,
-        config: config
-      )
-    }
+    return (request.results ?? []).map { HybridFace(face: $0, config: config) }
   }
 
-  private func resolveInputImage(
-    _ input: InputImage
-  ) throws -> String {
+  private func resolveInputImage(_ input: InputImage) throws -> String {
     switch input {
-      case .first(let uri):
-        return uri
-      case .third(let object):
-        return object.uri
-      case .second:
-        break
+      case .first(let uri): return uri
+      case .third(let object): return object.uri
+      case .second: break
     }
-
-    throw NSError(
-      domain: "HybridImageFaceDetector",
-      code: -1,
-      userInfo: [
-        NSLocalizedDescriptionKey:
-        "Invalid image type. Expected string or { uri }"
-      ]
-    )
+    throw RuntimeError.error(withMessage: "Invalid image type. Expected string or { uri }")
   }
 
-  private func createInputImage(
-    uri: String
-  ) throws -> UIImage {
+  private func createInputImage(uri: String) throws -> UIImage {
     guard let url = URL(string: uri) else {
-      throw NSError(
-        domain: "HybridImageFaceDetector",
-        code: -2,
-        userInfo: [
-          NSLocalizedDescriptionKey:
-          "Invalid image URI"
-        ]
-      )
+      throw RuntimeError.error(withMessage: "Invalid image URI")
     }
-
-    let image: UIImage
-    if url.isFileURL {
-      guard let loadedImage = UIImage(contentsOfFile: url.path)
-      else {
-        throw NSError(
-          domain: "HybridImageFaceDetector",
-          code: -3,
-          userInfo: [
-            NSLocalizedDescriptionKey:
-            "Failed to load image from file"
-          ]
-        )
-      }
-
-      image = loadedImage
-    } else {
-      guard let data = try? Data(contentsOf: url),
-            let loadedImage = UIImage(data: data)
-      else {
-        throw NSError(
-          domain: "HybridImageFaceDetector",
-          code: -4,
-          userInfo: [
-            NSLocalizedDescriptionKey:
-            "Failed to load image from URL"
-          ]
-        )
-      }
-
-      image = loadedImage
-    }
-
-    return image
+    if url.isFileURL, let image = UIImage(contentsOfFile: url.path) { return image }
+    if !url.isFileURL, let data = try? Data(contentsOf: url), let image = UIImage(data: data) { return image }
+    throw RuntimeError.error(withMessage: "Failed to load image from URI")
   }
 }
